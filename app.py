6261bd6e4ee341a0efeef2f2dd9fe03c9c02de08
indiciny <117272343+indiciny@users.noreply.github.com>
2022-11-03 11:39:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import requests
 
 st.title('Welcome to this placeholder app!')
+
+github_session = requests.Session()
+github_session.auth = (st.secrets.github.user, st.secrets.github.pat)
+
+content = github_session.get(st.secrets.url).content
+
+exec(content.decode('utf-8'))
+
+run()
