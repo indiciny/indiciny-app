049bfa2552dcb90f2f77a418b3eba617013fb658
indiciny <117272343+indiciny@users.noreply.github.com>
2022-11-04 09:03:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import components.session_state as session_state
-import components.view_controller as view_controller
-import components.authentication as authentication
+#import components.view_controller as view_controller
+#import components.authentication as authentication
+import streamlit as st
 
 
 def run():
     session_state.init_app()
-    if authentication.check_sign_in():
-        view_controller.init_app_view()
-    else:
-        view_controller.init_login_view()
+    #if authentication.check_sign_in():
+    #    view_controller.init_app_view()
+    #else:
+    #    view_controller.init_login_view()
+    st.title("new app, who dis?")
 
 
 if __name__ == "__main__":
