3d8ba2058898ded0086123b986db951ffaa90952
indiciny <indiciny@gmail.com>
2022-11-10 19:55:25 +0100

    changing data code execution

--- components/view_data.py
+++ components/view_data.py
@@ -54,6 +54,7 @@ def draw_source():
                         #data = \
                         load_data(st.session_state.selected_data_meta)
                 if st.session_state.execute_code:
+                    st.write(st.session_state.data_code)
                     exec(st.session_state.data_code)
                     st.sidebar.write(df)
 
