8ae0d9878846757a7de0c7af8388a5e6a4cc0ee3
indiciny <indiciny@gmail.com>
2022-11-11 15:49:58 +0100

    testing states

--- components/session_state.py
+++ components/session_state.py
@@ -37,7 +37,7 @@ def update_persistent_state(key, value):
 
 def reset_persistent_state():
     st.session_state.persistent_state = {
-        "data_selection": "",
+        "data_selection": st.session_state.data_meta[0],
         "data_params": "",
         "data_original": "",
         "data_current": "",
